b168ae9912425cc2edf58aa9a721cf41f40f4f43
EmmaK <emmaculatewkamau@gmail.com>
2022-02-24 19:19:57 +0300

    Add staticfiles_storages in settings

--- kivukoni/settings.py
+++ kivukoni/settings.py
@@ -171,6 +171,8 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
